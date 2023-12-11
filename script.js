06c66ed3a9c05d94baf4891580b8c7ce19923048
Gustavo Gonzalez <gagm0995@gmail.com>
2023-12-11 01:14:05 -0300

    app lista

---
+++ script.js
@@ -0,0 +1,54 @@
+let urlBase = 'https://api.openweathermap.org/data/2.5/weather'
+let api_key = 'ecc4a3fd54f6659e0f1c95dc7d9e67d3'
+let difKelvin = 273.15
+
+
+
+document.getElementById('botonBusqueda').addEventListener('click', () => {
+    const ciudad = document.getElementById('ciudadEntrada').value
+    if(ciudad){
+        fetchDatosClima(ciudad)
+    }
+})
+
+function fetchDatosClima(ciudad){
+    fetch(`${urlBase}?q=${ciudad}&appid=${api_key}`)
+    .then(data => data.json())
+    .then(data => mostrarDatosClima(data));
+    
+}
+
+function mostrarDatosClima (data){
+    
+    const divDatosClima = document.getElementById('datosClima');
+    divDatosClima.innerHTML= '';
+
+    const ciudadNombre = data.name
+    const paisNombre = data.sys.country
+    const temperatura = data.main.temp
+    const humedad = data.main.humidity
+    const descripcion = data.weather[0].description
+    const icono = data.weather[0].icon
+
+    const ciudadTitulo = document.createElement('h2')
+    ciudadTitulo.textContent =  `${ciudadNombre}, ${paisNombre}`
+
+    const temperaturaInfo = document.createElement('p')
+    temperaturaInfo.textContent = `La temperatura es: ${Math.floor(temperatura - difKelvin)} grados`
+
+    const humedadInfo = document.createElement('p')
+    humedadInfo.textContent = `La humedad es: ${humedad}%`
+
+    const iconInfo = document.createElement('img')
+    iconInfo.src = `https://openweathermap.org/img/wn/${icono}@2x.png`
+
+    const descripcionInfo = document.createElement('p')
+    descripcionInfo.textContent = `La descripcion meteorelogica es: ${descripcion}`
+
+    divDatosClima.appendChild(ciudadTitulo)
+    divDatosClima.appendChild(temperaturaInfo)
+    divDatosClima.appendChild(humedadInfo)
+    divDatosClima.appendChild(iconInfo)
+    divDatosClima.appendChild(descripcionInfo)
+}
+
